044397caef42635241d6a0e5df0955114a615396
PetkoGenchev <petko.genchev@yahoo.com>
2020-09-22 23:01:01 +0300

    test

--- OOP/Classes07-PokemonTrainer/Program.cs
+++ OOP/Classes07-PokemonTrainer/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Globalization;
 
 namespace Classes07_PokemonTrainer
 {
@@ -6,7 +12,22 @@ namespace Classes07_PokemonTrainer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                var inputData = Console.ReadLine()
+                    .Split()
+                    .ToArray();
+
+                if (inputData[0] == "Tournament")
+                {
+                    break;
+                }
+
+
+
+
+            }
+
         }
     }
 }
---
+++ OOP/Classes07-PokemonTrainer/Trainer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes07_PokemonTrainer
+{
+    public class Trainer
+    {
+        public string Name { get; set; }
+        public int NumberOfBadges { get; set; }
+
+        public int MyProperty { get; set; }
+
+    }
+}
