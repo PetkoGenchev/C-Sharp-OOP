f7b2b0fd352c28da81aa4ac2d3cfca196b193508
PetkoGenchev <petko.genchev@yahoo.com>
2020-09-22 15:22:00 +0300

    newTask

---
+++ OOP/Classes07-PokemonTrainer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Classes07_PokemonTrainer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
