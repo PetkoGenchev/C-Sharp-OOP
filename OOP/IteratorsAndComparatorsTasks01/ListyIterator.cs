10422427deb27f068001706934babcf81ad151b6
PetkoGenchev <petko.genchev@yahoo.com>
2020-10-16 16:05:39 +0300

    ienumerable01

---
+++ OOP/IteratorsAndComparatorsTasks01/ListyIterator.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparatorsTasks01
+{
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        public List<T> genericList { get; set; }
+        public int indexOfList { get; set; }
+
+
+        public ListyIterator(List<T> usingList)
+        {
+            this.genericList = usingList;
+        }
+
+
+        public bool Move(List<T> usingList,int indexOfList)
+        {
+            if (indexOfList >= usingList.Count)
+            {
+                return false;
+            }
+            else
+            {
+                indexOfList++;
+                return true;
+            }
+        }
+
+        public bool HasNext(List<T> usingList, int indexOfList)
+        {
+            if(indexOfList == usingList.Count - 1)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public void Print(List<T> usingList, int indexOfList)
+        {
+
+            try
+            {
+                Console.WriteLine(usingList[indexOfList]);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Invalid Operation!");
+            }
+        }
+
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OOP/IteratorsAndComparatorsTasks01/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Collections;
+
+namespace IteratorsAndComparatorsTasks01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            var listOfData = new List<string>();
+
+            var inputArray = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+            if (inputArray.Length > 1)
+            {
+                for (int i = 1; i < inputArray.Length; i++)
+                {
+                    listOfData.Add(inputArray[i]);
+                }
+            }
+            var listIterator = new ListyIterator<string>(listOfData);
+
+            var indexToUse = 0;
+
+            while (true)
+            {
+                var inputCommand = Console.ReadLine();
+                
+
+                if (inputCommand == "END")
+                {
+                    break;
+                }
+
+                if (inputCommand == "Move")
+                {
+                    Console.WriteLine(listIterator.Move(listOfData, indexToUse + 1));
+                    indexToUse++;
+                }
+                else if (inputCommand == "HasNext")
+                {
+                    Console.WriteLine(listIterator.HasNext(listOfData, indexToUse));
+                }
+                else if (inputCommand == "Print")
+                {
+                    listIterator.Print(listOfData, indexToUse);
+                }
+            }
+
+        }
+    }
+}
